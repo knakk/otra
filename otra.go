4270c32c39c056edde7d4278b358d0195fb96dde
Petter Goksøyr Åsen <boutrosboutrosboutros@gmail.com>
2016-10-21 20:12:27 +0200

    index scan autocomplete prototype

--- otra.go
+++ otra.go
@@ -1,5 +1,199 @@
 package main
 
-func main() {
+import (
+	"encoding/xml"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"strings"
 
+	"github.com/knakk/kbp/onix"
+	"github.com/knakk/kbp/onix/codes/list15"
+	"github.com/knakk/kbp/onix/codes/list163"
+	"github.com/knakk/kbp/onix/codes/list5"
+	"github.com/knakk/otra/db"
+)
+
+type ONIXMessage struct {
+	Product []*onix.Product
+}
+
+func main() {
+	dbFile := flag.String("db", "otra.db", "database file")
+	loadFile := flag.String("load", "", "load onix xml file into db")
+	listenAdr := flag.String("l", ":8765", "listening address")
+	flag.Parse()
+
+	db, err := db.Open(*dbFile, indexFn)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	if *loadFile != "" {
+		log.Printf("Attempting to parse onix product file")
+		b, err := ioutil.ReadFile("onix.xml")
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		var products ONIXMessage
+		if err := xml.Unmarshal(b, &products); err != nil {
+			log.Fatal(err)
+		}
+
+		log.Printf("Loading %d onix products into db", len(products.Product))
+		for _, p := range products.Product {
+			if _, err := db.Store(p); err != nil {
+				log.Fatal(err)
+			}
+		}
+	}
+
+	http.HandleFunc("/autocomplete/", func(w http.ResponseWriter, r *http.Request) {
+		paths := strings.Split(r.URL.Path, "/")
+		if len(paths) != 4 || paths[3] == "" {
+			http.Error(w, "usage: /autocomplete/index/query", http.StatusBadRequest)
+			return
+		}
+
+		hits, err := db.Scan(paths[2], paths[3], 10)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+
+		for _, hit := range hits {
+			fmt.Fprintln(w, hit)
+		}
+	})
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		if _, err := w.Write(page); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+	})
+
+	log.Printf("Starting otra server. Listening at %s", *listenAdr)
+	http.ListenAndServe(*listenAdr, nil)
+}
+
+func indexFn(p *onix.Product) (res []db.IndexEntry) {
+	for _, id := range p.ProductIdentifier {
+		if id.ProductIDType.Value == list5.ISBN13 {
+			res = append(res, db.IndexEntry{
+				Index: "isbn",
+				Term:  id.IDValue.Value,
+			})
+		}
+	}
+	for _, c := range p.DescriptiveDetail.Collection {
+		for _, t := range c.TitleDetail {
+			for _, tt := range t.TitleElement {
+				if tt.TitleText != nil {
+					res = append(res, db.IndexEntry{
+						Index: "collection",
+						Term:  tt.TitleText.Value,
+					})
+				}
+			}
+		}
+	}
+
+	for _, t := range p.DescriptiveDetail.TitleDetail {
+		if t.TitleType.Value == list15.DistinctiveTitleBookCoverTitleSerialTitleOnItemSerialContentItemOrReviewedResource {
+			res = append(res, db.IndexEntry{
+				Index: "title",
+				Term:  t.TitleElement[0].TitleText.Value,
+			})
+			if t.TitleElement[0].Subtitle != nil {
+				res = append(res, db.IndexEntry{
+					Index: "title",
+					Term:  t.TitleElement[0].Subtitle.Value,
+				})
+			}
+		}
+		if t.TitleType.Value == list15.TitleInOriginalLanguage {
+			res = append(res, db.IndexEntry{
+				Index: "title",
+				Term:  t.TitleElement[0].TitleText.Value,
+			})
+			if t.TitleElement[0].Subtitle != nil {
+				res = append(res, db.IndexEntry{
+					Index: "title",
+					Term:  t.TitleElement[0].Subtitle.Value,
+				})
+			}
+		}
+	}
+
+	/*
+		for _, l := range p.DescriptiveDetail.Language {
+			if l.LanguageRole.Value == list22.LanguageOfText {
+				b.Language = list74.MustItem(l.LanguageCode.Value, codes.Norwegian).Label
+			} else if l.LanguageRole.Value == list22.OriginalLanguageOfATranslatedText {
+				b.OriginalLanguage = list74.MustItem(l.LanguageCode.Value, codes.Norwegian).Label
+			}
+		}*/
+
+	for _, p := range p.PublishingDetail.Publisher {
+		res = append(res, db.IndexEntry{
+			Index: "publisher",
+			Term:  p.PublisherName.Value,
+		})
+		break
+	}
+	for _, d := range p.PublishingDetail.PublishingDate {
+		if d.PublishingDateRole.Value == list163.PublicationDate {
+			res = append(res, db.IndexEntry{
+				Index: "year",
+				Term:  d.Date.Value,
+			})
+			break
+		}
+	}
+
+	for _, s := range p.DescriptiveDetail.Subject {
+		for _, st := range s.SubjectHeadingText {
+			res = append(res, db.IndexEntry{
+				Index: "subject",
+				Term:  st.Value,
+			})
+		}
+	}
+
+	for _, c := range p.DescriptiveDetail.Contributor {
+		for _, role := range c.ContributorRole {
+			var agent string
+			if c.PersonNameInverted != nil {
+				agent = c.PersonNameInverted.Value
+			} else if c.PersonName != nil {
+				agent = c.PersonName.Value
+			}
+			res = append(res, db.IndexEntry{
+				Index: "agent",
+				Term:  agent,
+			})
+			var roleIndex string
+			switch role.Value {
+			case "A01":
+				roleIndex = "author"
+			case "A112":
+				roleIndex = "illustrator"
+			case "B01":
+				roleIndex = "editor"
+			case "B06":
+				roleIndex = "translator"
+			default:
+				roleIndex = "role" + role.Value
+			}
+			res = append(res, db.IndexEntry{
+				Index: roleIndex,
+				Term:  agent,
+			})
+		}
+	}
+
+	return res
 }
---
+++ page.go
@@ -0,0 +1,91 @@
+package main
+
+var page = []byte(`
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Library Gutenberg</title>
+	<style>
+		*              { box-sizing: border-box }
+		body           { margin:1em auto; max-width:40em; padding: 0 .62em; font:1.2em/1.62em sans-serif; }
+		a, a:visited   { color: blue; }
+		h1, h2, h3, h4 { line-height:1.2em; }
+		label          { display: block; font-weight: bold; font-size: 80%; }
+		input          { padding: .2em .62em; font-size: 100% }
+		.row           { clear: both }
+		.col           { width: 50%; float: left }
+
+		@media print { body { max-width:none } }
+	</style>
+</head>
+<body>
+	<article>
+		<header>
+			<h1>Otra</h1>
+		</header>
+		<section class="row">
+			<div class="col">
+				<datalist id="indexes">
+					<option>agent</option>
+					<option>author</option>
+					<option>collection</option>
+					<option>editor</option>
+					<option>illustrator</option>
+					<option>isbn</option>
+					<option>subject</option>
+					<option>title</option>
+					<option>translator</option>
+					<option>year</option>
+				</datalist>
+				<datalist id="hits"></datalist>
+				<label>Index</label>
+				<input id="index" type="text" list="indexes" />
+			</div>
+			<div class="col">
+				<label>Query</label>
+				<input id="query" type="text" list="hits" />
+			</div>
+		</section>
+		<section>
+		</section>
+	</article>
+	<script>
+		function keyPressed(event) {
+			var index = document.getElementById('index').value
+			var query = document.getElementById('query').value
+			if (index == '' || query === '') {
+				return
+			}
+			var req = new XMLHttpRequest()
+			req.open('GET', '/autocomplete/'+index+'/'+query, true)
+			req.onload = function() {
+				if (req.status >= 200 && req.status < 400) {
+					var hits = req.responseText.split('\n')
+					var hitsList = document.getElementById('hits')
+					while (hitsList.firstChild) {
+					    hitsList.removeChild(hitsList.firstChild)
+					}
+					hits.forEach(function(hit) {
+						var option = document.createElement('option')
+				        option.value = hit;
+				        hitsList.appendChild(option);
+					})
+				} else {
+					console.log(req.status)
+			  	}
+			}
+
+			req.onerror = function() {
+				console.log("connection error")
+			}
+			req.send()
+		}
+		var input = document.getElementById('query')
+		input.addEventListener('keyup', keyPressed)
+	</script>
+</body>
+</html>
+
+`)
