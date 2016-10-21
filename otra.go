07fce254974514393870ae4b28d39b9e5c6555db
Petter Goksøyr Åsen <boutrosboutrosboutros@gmail.com>
2016-10-21 20:47:28 +0200

    Add query endpoint

--- otra.go
+++ otra.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"encoding/xml"
 	"flag"
 	"fmt"
@@ -10,9 +11,14 @@ import (
 	"strings"
 
 	"github.com/knakk/kbp/onix"
+	"github.com/knakk/kbp/onix/codes"
 	"github.com/knakk/kbp/onix/codes/list15"
+	"github.com/knakk/kbp/onix/codes/list150"
 	"github.com/knakk/kbp/onix/codes/list163"
+	"github.com/knakk/kbp/onix/codes/list17"
+	"github.com/knakk/kbp/onix/codes/list22"
 	"github.com/knakk/kbp/onix/codes/list5"
+	"github.com/knakk/kbp/onix/codes/list74"
 	"github.com/knakk/otra/db"
 )
 
@@ -71,6 +77,34 @@ func main() {
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		if _, err := w.Write(page); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+	})
+
+	http.HandleFunc("/query/", func(w http.ResponseWriter, r *http.Request) {
+		paths := strings.Split(r.URL.Path, "/")
+		if len(paths) != 4 || paths[3] == "" {
+			http.Error(w, "usage: /query/index/query", http.StatusBadRequest)
+			return
+		}
+
+		ids, err := db.Query(paths[2], paths[3], 10)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		var hits []Result
+		for _, id := range ids {
+			p, err := db.Get(id)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+			hits = append(hits, extractRes(p))
+		}
+
+		if err := json.NewEncoder(w).Encode(&hits); err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 		}
 	})
@@ -195,5 +229,100 @@ func indexFn(p *onix.Product) (res []db.IndexEntry) {
 		}
 	}
 
+	return res
+}
+
+type Contrib struct {
+	Role  string
+	Agent string
+}
+
+type Result struct {
+	Contributors     []Contrib
+	Collection       []string
+	Subjects         []string
+	Title            string
+	Subtitle         string
+	OriginalTitle    string
+	Language         string
+	OriginalLanguage string
+	ISBN             string
+	Format           string
+	Publisher        string
+	PublishedYear    string
+}
+
+func extractRes(p *onix.Product) (res Result) {
+	for _, id := range p.ProductIdentifier {
+		if id.ProductIDType.Value == list5.ISBN13 {
+			res.ISBN = id.IDValue.Value
+		}
+	}
+	res.Format = list150.MustItem(p.DescriptiveDetail.ProductForm.Value, codes.Norwegian).Label
+	for _, c := range p.DescriptiveDetail.Collection {
+		for _, t := range c.TitleDetail {
+			for _, tt := range t.TitleElement {
+				if tt.TitleText != nil {
+					res.Collection = append(res.Collection, tt.TitleText.Value)
+				}
+			}
+		}
+	}
+
+	for _, t := range p.DescriptiveDetail.TitleDetail {
+		if t.TitleType.Value == list15.DistinctiveTitleBookCoverTitleSerialTitleOnItemSerialContentItemOrReviewedResource {
+			res.Title = t.TitleElement[0].TitleText.Value
+			if t.TitleElement[0].Subtitle != nil {
+				res.Subtitle = t.TitleElement[0].Subtitle.Value
+			}
+		}
+		if t.TitleType.Value == list15.TitleInOriginalLanguage {
+			res.OriginalTitle = t.TitleElement[0].TitleText.Value
+			if t.TitleElement[0].Subtitle != nil {
+				res.OriginalTitle = fmt.Sprintf("%s : %s", res.OriginalTitle, t.TitleElement[0].Subtitle.Value)
+			}
+		}
+	}
+
+	for _, l := range p.DescriptiveDetail.Language {
+		if l.LanguageRole.Value == list22.LanguageOfText {
+			res.Language = list74.MustItem(l.LanguageCode.Value, codes.Norwegian).Label
+		} else if l.LanguageRole.Value == list22.OriginalLanguageOfATranslatedText {
+			res.OriginalLanguage = list74.MustItem(l.LanguageCode.Value, codes.Norwegian).Label
+		}
+	}
+
+	for _, p := range p.PublishingDetail.Publisher {
+		res.Publisher = p.PublisherName.Value
+		break
+	}
+	for _, d := range p.PublishingDetail.PublishingDate {
+		if d.PublishingDateRole.Value == list163.PublicationDate {
+			res.PublishedYear = d.Date.Value
+			break
+		}
+		// TODO list163.DateOfFirstPublication ?
+	}
+
+	for _, s := range p.DescriptiveDetail.Subject {
+		for _, st := range s.SubjectHeadingText {
+			res.Subjects = append(res.Subjects, st.Value)
+		}
+	}
+
+	for _, c := range p.DescriptiveDetail.Contributor {
+		for _, role := range c.ContributorRole {
+			contrib := Contrib{
+				Role: list17.MustItem(role.Value, codes.Norwegian).Label,
+			}
+			if c.PersonNameInverted != nil {
+				contrib.Agent = c.PersonNameInverted.Value
+			} else if c.PersonName != nil {
+				contrib.Agent = c.PersonName.Value
+			}
+			res.Contributors = append(res.Contributors, contrib)
+		}
+
+	}
 	return res
 }
